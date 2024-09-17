73fafe44764ec028bfbc726e3188821aab7dc661
Luka Micovic <lukamicovic995@gmail.com>
2024-09-17 20:21:20 +0200

    Created Movies Tables and Filtration Form in Administrator Movies Component

--- src/app/components/administrative-tools/administrator-movies/administrator-movies.component.ts
+++ src/app/components/administrative-tools/administrator-movies/administrator-movies.component.ts
@@ -1,19 +1,42 @@
 import { Component } from '@angular/core';
 import { mainContentAnimation } from '../../../other/animations/sidebar.animation';
+import { expandCollapse } from '../../../other/animations/expandCollapse.animation';
 
 @Component({
   selector: 'app-administrator-movies',
   templateUrl: './administrator-movies.component.html',
   styleUrl: './administrator-movies.component.css',
   animations: [
-    mainContentAnimation
+    mainContentAnimation,
+    expandCollapse
   ]
 })
 export class AdministratorMoviesComponent {
 
+  
+
   constructor() {
 
   }
+
+  //----------------------------------------------------------------------------------------------------
+  // Filter Panel
+  //----------------------------------------------------------------------------------------------------
+  isFilterPanelToggled: boolean = false;
+  onFilter() {
+    this.isFilterPanelToggled = !this.isFilterPanelToggled;
+  }
+
+  //----------------------------------------------------------------------------------------------------
+  // Pagination
+  //----------------------------------------------------------------------------------------------------
+  first: number = 0;
+  rows: number = 5;                       // Number of rows in one Page in Paggination
+  onPaginationChange(event: any) {
+    this.first = event.first;
+    this.rows = event.rows;
+  }
+  //----------------------------------------------------------------------------------------------------
 
   //----------------------------------------------------------------------------------------------------
   // Used to transfer isSidePanelToggled data from topbar.components.ts to sidebar.component.ts
