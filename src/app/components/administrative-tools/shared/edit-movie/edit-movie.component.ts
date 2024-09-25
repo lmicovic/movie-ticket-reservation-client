f30676b45a5f7259534b1ac3c63e919ffe8d8065
Luka Micovic <lukamicovic995@gmail.com>
2024-09-25 15:03:24 +0200

    Created Update Movie Panel and Statistic Panel in Edit Movie Component

--- src/app/components/administrative-tools/shared/edit-movie/edit-movie.component.ts
+++ src/app/components/administrative-tools/shared/edit-movie/edit-movie.component.ts
@@ -1,20 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { StatisticCardModelDTO } from '../../../../other/models/statistic-card-model/statistic-card-model.interface';
+import { StatisticCardModel } from '../../../../other/models/statistic-card-model/statistic-card-model.class';
+import { faMoneyBill, faTag, faUsersViewfinder } from '@fortawesome/free-solid-svg-icons';
+import { slideDown, slideRight } from '../../../../other/animations/slide.animation';
+
 
 @Component({
   selector: 'app-edit-movie',
   templateUrl: './edit-movie.component.html',
-  styleUrl: './edit-movie.component.css'
+  styleUrl: './edit-movie.component.css',
+  animations: [
+    slideDown, slideRight
+  ]
 })
 export class EditMovieComponent implements OnInit{
 
-  constructor(private activatedRoute: ActivatedRoute, public router: Router) {
+  defaultImage: string = "../../../../../assets/images/image-placeholder-1.jpg";
+
+  private movieId!: number;
+  
+  //-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  // Statistic Cards
+  //-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  profitCardValues: StatisticCardModelDTO = new StatisticCardModel("Profit", "$2.150", "+$1.500", "since last week", faMoneyBill, "#f97316", "#feddc7");
+  viewsCardValues: StatisticCardModelDTO = new StatisticCardModel("Views", "152", "+24", "since last week", faUsersViewfinder, "#3b82f6", "#d0e1fd");
+  reservationsCardValues: StatisticCardModelDTO = new StatisticCardModel("Reservations", "256", "+10", "since last week", faTag, "#a855f7", "#ead6fd");
+  //-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+  constructor(private activatedRoute: ActivatedRoute) {
 
   }
 
   ngOnInit(): void {
 
-    
+    // Get MovieId from URL Variable Parameter
+    this.movieId = +(this.activatedRoute.snapshot.paramMap.get("movieId") as String);
 
   }
 
