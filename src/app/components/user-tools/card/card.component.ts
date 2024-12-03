ca3627492ee0e3d4a6d87490006b9c9a8985e5e6
Luka Micovic <lukamicovic995@gmail.com>
2024-12-03 14:58:32 +0100

    Created Card Component

--- src/app/components/user-tools/card/card.component.ts
+++ src/app/components/user-tools/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 })
 export class CardComponent {
 
+  @Input("cardTitle")
+  cardTitle: string = "";
+  
   constructor() {
     
   }
